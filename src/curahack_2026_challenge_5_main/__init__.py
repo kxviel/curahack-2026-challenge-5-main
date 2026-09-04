5f902c79682bdbd5a40e5cafaaba21663c1c0af7
Kevin D Goveas <kevdanngovead@gmail.com>
2026-09-04 19:58:54 +0200

    fix: path issues

--- src/curahack_2026_challenge_5_main/__init__.py
+++ src/curahack_2026_challenge_5_main/__init__.py
@@ -3,35 +3,28 @@
 from __future__ import annotations
 
 import sys
+from pathlib import Path
+from typing import Literal
 
 from .gai_cal import main as run_gai
 
 __all__ = ["main"]
 
+PROJECT_ROOT = Path.cwd().parent
+
 
 def main() -> None:
-    meta_path = "./datasets/processed/"
-    otu_path = "./datasets/processed/"
-    output_dir = "./result/"
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 2 or sys.argv[1].lower() not in ("agp", "ggmp"):
         print("Invalid arguments!")
         print("Usage: uv run gai-cal ggmp | uv run gai-cal agp")
         return
 
-    if sys.argv[1] == "agp":
-        meta_path += "AGP/meta.tsv"
-        otu_path += "AGP/otu.tsv"
-        output_dir += "agp/"
+    type DATASET = Literal["AGP", "GGMP"]
+    dataset: DATASET = "AGP" if sys.argv[1].lower() == "agp" else "GGMP"
 
-    elif sys.argv[1] == "ggmp":
-        meta_path += "GGMP/meta.tsv"
-        otu_path += "GGMP/otu.tsv"
-        output_dir += "ggmp/"
+    meta_path = PROJECT_ROOT / "datasets/processed" / dataset / "meta.tsv"
+    otu_path = PROJECT_ROOT / "datasets/processed" / dataset / "otu.tsv"
+    output_path = PROJECT_ROOT / "result" / dataset
 
-    else:
-        print("Invalid arguments!")
-        print("Usage: uv run gai-cal ggmp | uv run gai-cal agp")
-        return
-
-    run_gai(meta_path, otu_path, output_dir)
+    run_gai(meta_path, otu_path, output_path)
